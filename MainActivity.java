53a915c0d27afdeec5583c58e7351c26999c7ac7
ranarn <ranran@qq.com>
2015-06-23 15:44:29 +0800

    初始化导入

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+
+package com.example.aaproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        System.out.println("333333333333333333333333333");
+        System.out.println("4444444444444443333");
+        
+    }
+}
