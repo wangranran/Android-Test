7a19ee801155d1143005cee6f537db552a7e24aa
ranarn <ranran@qq.com>
2015-06-23 15:48:09 +0800

    第二次提交

--- MainActivity.java
+++ MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+//这是一个Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
